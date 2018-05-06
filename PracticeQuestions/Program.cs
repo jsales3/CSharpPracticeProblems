5c7052c2920a3679c6446f778a53be8ac1db0207
jsales3 <jovonsales@gmail.com>
2018-05-06 17:45:04 -0400

    first commit

---
+++ PracticeQuestions/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PracticeQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FizzBuzz Program
+
+            //int num = 100;
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine("FizzBuzz");
+            //    }
+            //    else if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else
+            //        Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+
+            //
+
+            //int n;
+
+            //n = int.Parse(Console.ReadLine());
+
+            //for(int i = 1; i <= n; i++)
+            //{
+            //    for(int j )
+            //    Console.Write(" ");
+            //    if (i == n)
+            //    {
+            //        Console.Write("#");
+            //        Console.WriteLine();
+            //    }
+
+
+
+            //}
+
+            //string[] ar = new string[3];
+            //int[] A = new int[3];
+            //int[] B = new int[3];
+
+            //int Alice = 0;
+            //int Bob = 0;
+
+            //ar = Console.ReadLine().Split();
+            //for (int i = 0; i < A.Length; i++)
+            //{
+
+            //    A[i] = int.Parse(ar[i]);
+
+
+            //}
+
+            //ar = Console.ReadLine().Split();
+            //for (int i = 0; i < A.Length; i++)
+            //{
+
+            //    B[i] = int.Parse(ar[i]);
+
+
+            //}
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    if (A[i] > B[i])
+            //    {
+            //        Alice++;
+            //    }
+            //    else if (A[i] < B[i])
+            //    {
+            //        Bob++;
+            //    }
+            //}
+
+            //Console.WriteLine(Alice + " " + Bob);
+            //Console.ReadLine();
+
+
+            //    int n;
+            //    int d;
+
+            //    string[] ar = new String[2];
+
+
+
+            //ar = Console.ReadLine().Split();
+
+            //    n = int.Parse(ar[0]);
+            //    d = int.Parse(ar[1]);
+
+            //    int[] num = new int[n];
+
+
+            //    ar = Console.ReadLine().Split();
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        num[i] = int.Parse(ar[i]);
+
+            //    }
+
+            //    int [] rotate = new int[n];
+
+            //    for(int i = 0; i < d; i++)
+            //    {
+            //        rotate[4] = num[i];
+            //        for(int j= 4; j > 0; j--)
+            //        {
+            //            rotate[] = n;
+            //        }
+            //    }
+
+            //    //for (int i = 0; i < d; i++)
+            //    //{
+            //    //    rotate[i] = num[i];
+
+            //    //}
+            //    //num[0] = num[4];
+            //    //for (int i = 0; i < d; i++)
+            //    //{
+            //    //    num[i+1] = rotate[i];
+
+
+            //}
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write(num[i] + " ");
+
+
+            //}
+
+
+
+            //Console.ReadLine();
+            //try
+            //{
+            //string answer = "NO";
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr = new int[n];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+
+
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int k = int.Parse(Console.ReadLine());
+
+            //foreach (int num in arr)
+            //{
+            //    if (num == k)
+            //    {
+            //        answer = "YES";
+            //    }
+
+            //}
+            //Console.WriteLine(answer);
+            //}
+            //catch(Exception )
+            //{
+            //    Console.WriteLine("error");
+            //}
+
+            //List<int> num = new List<int>();
+            //int[] arr;
+            //int l = int.Parse(Console.ReadLine());
+            //int r = int.Parse(Console.ReadLine());
+
+            //for (int i = l; i <= r; i++)
+            //{
+            //    if(i % 2 != 0)
+            //    {
+            //        num.Add(i);
+                    
+
+            //    }
+                
+
+            //}
+            //arr = num.ToArray();
+
+            
+            //for (int i=0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i] + " ");
+            //}
+
+            
+
+
+
+        }
+
+    }
+}
